bc6e5867b24ae662d44296d0cbf8651230057ee6
roncortez <ronny.cortez12@gmail.com>
2025-12-03 09:49:02 -0500

    Create cart context

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -2,6 +2,21 @@ export default function Cart() {
     return (
         <div className="min-h-screen bg-gray-50">
             <h1>Detalle</h1>
+
+            <div className="grid grid-cols-4 gap-4 font-medium">
+                <div>Producto</div>
+                <div>Precio unitario</div>
+                <div>Cantidad</div>
+                <div>Precio total</div>
+            </div>
+            <div className="grid grid-cols-4 gap-4 items-center py-3 border-b">
+                <div className="font-medium">Laptop ASUS</div>
+                <div>$1200</div>
+                <div>2</div>
+                <div>$2400</div>
+            </div>
+
+
             <div className="flex flex-col items-center gap-2">
                 <label>¿Tienes un cupón de descuento?</label>
                 <input className="border border-gray-300 rounded-lg p-2" type="text" placeholder="CUPÓN" />
